57d6aed2111844693e4d272f9fba9f91d72d2fde
skmitra <mitrakumarsujan@gmail.com>
2021-05-09 00:11:04 +0530

    Added consumer app

---
+++ kafka-consumer/src/main/java/com/github/mitrakumarsujan/kafkaconsumer/KafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.github.mitrakumarsujan.kafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerApplication.class, args);
+	}
+
+}
